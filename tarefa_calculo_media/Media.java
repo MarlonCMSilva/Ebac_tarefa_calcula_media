77724cc3d551a69c4ba5a99a81e410b790a02c56
Marlon Conceição <marlon.conceicao.0308@gmail.com>
2024-03-05 19:17:59 -0300

    first commit

---
+++ tarefa_calculo_media/Media.java
@@ -0,0 +1,17 @@
+public class Media {
+
+    public static void main(String args[]){
+        mediaEscolar();
+    }
+
+    private  static void  mediaEscolar() {
+       int nota1 = 7;
+       int nota2 = 7;
+       int nota3 = 7;
+       int nota4 = 7;
+       int media = (nota1 + nota2 + nota3 + nota4) / 4;
+       System.out.println("Sua media Escolar é: " + media);
+    }
+}
+
+
